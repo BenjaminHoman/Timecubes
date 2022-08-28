7cffdaa5946244d20252a649a9da107f1a854b2e
Ben Homan <benjaminhoman10100@gmail.com>
2022-08-28 16:51:04 -0400

    basic scene

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+app.use(express.static('assets'))
+
+// Ignore Favicon. For now it is annoying.
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
